2328fe9e510be11c7fcdb7112dfbcd1897b74218
MyRemKi <remi.kalkan01@hotmail.com>
2026-04-14 13:26:05 +0200

    first commit

---
+++ mon-app/src/App.vue
@@ -0,0 +1,17 @@
+Remi Kalkan
+14/04/2026
+
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+const buildId = import.meta.env.VITE_BUILD_ID ?? 'local'
+const today = new Date().toLocaleDateString('fr-FR')
+</script>
+
+<template>
+  <header>
+    <p>Nom : Remi Kalkan</p>
+    <p>Date : {{ today }}</p>
+    <p>Build : {{ buildId }}</p>
+  </header>
+</template>
